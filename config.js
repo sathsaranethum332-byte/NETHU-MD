726730f0f1ad8386aa7c1b00d533e48d7c0b598d
sathsaranethum332-byte <sathsaranethum332@gmail.com>
2026-03-29 12:55:37 +0530

    Update ALIVE_IMG URL in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sathsaranethum332-byte/NETHU-MD/blob/main/ChatGPT%20Image%20Mar%2029,%202026,%2012_53_26%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "🔥 *NETHUM-MD*\n📱 0741962754\n\nඔයාට ඕනේ service ekak nam msg ekak danna!",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
